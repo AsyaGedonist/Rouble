1c3f3bae54a970c88a2b228daeed12dffc1d96aa
Anastasiia Tamonova <ice_asa@mail.ru>
2022-06-20 22:43:17 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double count = 100;
+        double pay = 1100;
+        int bonus = pay > 1000 ? (int)pay/100 : 0;
+
+        count = count + pay + bonus;
+
+        System.out.println(count);
+    }
+}
